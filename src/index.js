9a4a6710094b77775d91103b82b5e274a8010ce9
Jacob Aguilar <aguilar.campos.jacob@gmail.com>
2024-10-04 00:59:07 -0300

    Agrega cors

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import express from "express";
+import cors from "cors";
+import bodyParser from 'body-parser';
 import { PORT } from "./config.js";
 import reservesRoutes from "./routes/reserves.routes.js";
 
 const app = express();
 
+app.use(cors());
+
+// O bien, para limitarlo solo a tu origen (front-end)
+app.use(cors({
+    origin: 'http://localhost:4321'  // Cambia esto por la URL de tu front-end
+}));
+
+app.use(bodyParser.json({ limit: '1mb' }));
+app.use(bodyParser.urlencoded({ limit: '1mb', extended: true }));
 app.use(express.json());
 app.use(reservesRoutes)
 
