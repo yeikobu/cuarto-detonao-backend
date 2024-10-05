7f68a2c23649bbe1a5dd6ee3f622488cb96cb5da
Jacob Aguilar <aguilar.campos.jacob@gmail.com>
2024-10-04 23:22:21 -0300

    Agrega conexión con supabase

--- src/db.js
+++ src/db.js
@@ -2,10 +2,16 @@ import pg from "pg";
 import { DB_USER, DB_HOST, DB_DATABASE, DB_PASSWORD, DB_PORT } from "./config.js";
 
 export const pool = new pg.Pool({
-  user: DB_USER,
-  host: DB_HOST,
-  database: DB_DATABASE,
-  password: DB_PASSWORD,
-  port: DB_PORT,
+  connectionString: process.env.DB_HOST,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
+pool.on('connect', () => {
+  console.log('Conectado a la base de datos de Supabase');
+});
+
+pool.on('error', (err) => {
+  console.error('Error en la conexión a la base de datos:', err);
+});
